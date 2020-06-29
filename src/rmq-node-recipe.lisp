bec8c4538c259394083447b277fd9a23015e586b
Paul Ricks <paul.ricks@vroom.com>
2020-06-29 15:53:41 -0400

    fix rmq-node-recipe constructor

--- src/rmq-node-recipe.lisp
+++ src/rmq-node-recipe.lisp
@@ -21,8 +21,11 @@
 
 (defun build-rmq-node-recipe (node-type transform-fn source-queue dest-queue
                               &optional batch-size)
-  (make-instance 'rmq-node-recipe :type node-type :transform-fn transform-fn :batch-size batch-size
-                 :source source-queue :dest dest-queue))
+  (let ((args `(rmq-node-recipe :type ,node-type :transform-fn ,transform-fn
+                                :source ,source-queue :dest ,dest-queue)))
+    (if batch-size (setf args (append args `(:batch-size ,batch-size))))
+    (print args)
+    (apply #'make-instance args)))
 
 (defmethod name-fail-queue ((recipe rmq-node-recipe))
   (format nil "~a-fail" (node-recipe/type recipe)))
