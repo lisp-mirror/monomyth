a77d6b893ce58d6a9e87b4a7c1b8b2bd5348f496
Paul Ricks <paul.ricks@beyondidentity.com>
2021-11-29 08:24:11 -0500

    feature (life cycle): support for start-fn/stop-fn
    Added support in the dsl for user supplied startup and shutdown
    extensions.
    
    ISSUE: #94

--- tests/dsl.lisp
+++ tests/dsl.lisp
@@ -3,6 +3,15 @@
   (:shadow :closer-mop))
 (in-package :monomyth/tests/dsl)
 
+(v:output-here *terminal-io*)
+(defvar *test-context*)
+
+(setup
+  (setf *test-context* (pzmq:ctx-new)))
+
+(teardown
+  (pzmq:ctx-destroy *test-context*))
+
 (define-system (:pull-first nil)
   (:name dsl-pull-test :fn nil :batch-size 1))
 
@@ -18,3 +27,30 @@
   (let ((tnode (build-dsl-place-test nil nil nil *rmq-host* *rmq-port* *rmq-user* *rmq-pass*)))
     (ng (node/place-destination tnode))
     (ng (rmq-node/dest-queue tnode))))
+
+(defvar rmq-test-var)
+(define-rmq-node fn-node-test nil 1
+  :start-fn #'(lambda () (setf rmq-test-var t))
+  :stop-fn #'(lambda () (setf rmq-test-var nil)))
+
+(deftest fn-rmq-node
+  (let ((tnode (build-fn-node-test (format nil "testnode-~a" (get-universal-time))
+                                   nil :test *rmq-host* *rmq-port* *rmq-user* *rmq-pass*)))
+    (startup tnode *test-context* nil nil)
+    (ok rmq-test-var)
+    (shutdown tnode)
+    (ng rmq-test-var)))
+
+(defvar system-test-var)
+(define-system ()
+  (:name fn-system-test :batch-size 1
+        :start-fn #'(lambda () (setf system-test-var t))
+        :stop-fn #'(lambda () (setf system-test-var nil))))
+
+(deftest fn-system
+  (let ((tnode (build-fn-system-test (format nil "testnode-~a" (get-universal-time))
+                                     nil :test *rmq-host* *rmq-port* *rmq-user* *rmq-pass*)))
+    (startup tnode *test-context* nil nil)
+    (ok system-test-var)
+    (shutdown tnode)
+    (ng system-test-var)))
